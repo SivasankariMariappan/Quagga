4acc5fbc49ae468f143fa97156e0aa6a2e6c3c6f
Sivasankari <d906230@team.telstra.com>
2020-08-25 18:21:46 +0530

    Quagga-react integration

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,82 @@
-import React, { useState, useRef } from 'react';
-import Scanner from './Scanner';
-import Result from './Result';
+import React, { Component } from "react";
+import Scanner from "./Scanner";
+import Result from "./Result";
+import { isMobile } from "react-device-detect";
+import "./App.css";
 
-const App = () => {
-    const [scanning, setScanning] = useState(false);
-    const [results, setResults] = useState([]);
-    const scannerRef = useRef(null);
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scanning: false,
+      results: []
+    };
+    this.scannerRef = React.createRef();
+  }
 
+  setScanning = scanning => {
+    this.setState({ scanning });
+  };
+
+  setResults = result => {
+    this.setState({ results: [...this.state.results, result] });
+  };
+  render() {
+    const { scanning, results } = this.state;
     return (
-        <div>
-            <button onClick={() => setScanning(!scanning) }>{scanning ? 'Stop' : 'Start'}</button>
-            <ul className="results">
-                {results.map((result) => (result.codeResult && <Result key={result.codeResult.code} result={result} />))}
-            </ul>
-            <div ref={scannerRef} style={{position: 'relative', border: '3px solid red'}}>
-                {/* <video style={{ width: window.innerWidth, height: 480, border: '3px solid orange' }}/> */}
-                <canvas className="drawingBuffer" style={{
-                    position: 'absolute',
-                    top: '0px',
-                    // left: '0px',
-                    // height: '100%',
-                    // width: '100%',
-                    border: '3px solid green',
-                }} width="640" height="480" />
-                {scanning ? <Scanner scannerRef={scannerRef} onDetected={(result) => setResults([...results, result])} /> : null}
-            </div>
+      <div>
+        <button onClick={() => this.setScanning(!scanning)}>
+          {scanning ? "Stop" : "Start"}
+        </button>
+        <ul className="results">
+          {results.map(
+            result =>
+              result.codeResult && (
+                <Result key={result.codeResult.code} result={result} />
+              )
+          )}
+        </ul>
+
+        <div
+          id="interactive"
+          class="viewport"
+          ref={this.scannerRef}
+          style={{
+            position: "relative",
+            border: "3px solid red",
+            height: isMobile ? "100%" : "332px",
+            width: isMobile ? "100%" : "545px"
+          }}
+        >
+          <video
+            autoplay="true"
+            preload="auto"
+            src=""
+            muted="true"
+            playsinline="true"
+          ></video>
+          {/* <video style={{ width: window.innerWidth, height: 480, border: '3px solid orange' }}/> */}
+          <canvas
+            className="drawingBuffer"
+            style={{
+              position: "absolute",
+              top: "0px",
+              // left: '0px',
+              // height: '100%',
+              // width: '100%',
+              border: "3px solid green"
+            }}
+          />
+          {scanning ? (
+            <Scanner
+              scannerRef={this.scannerRef}
+              onDetected={result => this.setResults(result)}
+            />
+          ) : null}
         </div>
+      </div>
     );
-};
+  }
+}
 
 export default App;
--- src/components/Scanner.js
+++ src/components/Scanner.js
@@ -1,128 +1,36 @@
-import React, { useCallback, useLayoutEffect } from 'react';
-import PropTypes from 'prop-types';
-import Quagga from '@ericblade/quagga2';
+import { Component } from "react";
+import PropTypes from "prop-types";
+import Quagga from "@ericblade/quagga2";
 
 function getMedian(arr) {
-    arr.sort((a, b) => a - b);
-    const half = Math.floor(arr.length / 2);
-    if (arr.length % 2 === 1) {
-        return arr[half];
-    }
-    return (arr[half - 1] + arr[half]) / 2;
+  arr.sort((a, b) => a - b);
+  const half = Math.floor(arr.length / 2);
+  if (arr.length % 2 === 1) {
+    return arr[half];
+  }
+  return (arr[half - 1] + arr[half]) / 2;
 }
 
 function getMedianOfCodeErrors(decodedCodes) {
-    const errors = decodedCodes.filter(x => x.error !== undefined).map(x => x.error);
-    const medianOfErrors = getMedian(errors);
-    return medianOfErrors;
+  const errors = decodedCodes
+    .filter(x => x.error !== undefined)
+    .map(x => x.error);
+  const medianOfErrors = getMedian(errors);
+  return medianOfErrors;
 }
 
 const defaultConstraints = {
-    width: 640,
-    height: 480,
+  width: 1280, //640, 800, 1280 1600 1920,
+  height: 720 //480   600   720 960 1080
 };
 
 const defaultLocatorSettings = {
-    patchSize: 'medium',
-    halfSample: true,
+  patchSize: "medium",
+  halfSample: true
 };
 
-const defaultDecoders = ['ean_reader'];
-
-const Scanner = ({
-    onDetected,
-    scannerRef,
-    onScannerReady,
-    cameraId,
-    facingMode,
-    constraints = defaultConstraints,
-    locator = defaultLocatorSettings,
-    numOfWorkers = navigator.hardwareConcurrency || 0,
-    decoders = defaultDecoders,
-    locate = true,
-}) => {
-    const errorCheck = useCallback((result) => {
-        if (!onDetected) {
-            return;
-        }
-        const err = getMedianOfCodeErrors(result.codeResult.decodedCodes);
-        // if Quagga is at least 75% certain that it read correctly, then accept the code.
-        if (err < 0.25) {
-            onDetected(result.codeResult.code);
-        }
-    }, [onDetected]);
-
-    const handleProcessed = (result) => {
-        const drawingCtx = Quagga.canvas.ctx.overlay;
-        const drawingCanvas = Quagga.canvas.dom.overlay;
-        drawingCtx.font = "24px Arial";
-        drawingCtx.fillStyle = 'green';
-
-        if (result) {
-            // console.warn('* quagga onProcessed', result);
-            if (result.boxes) {
-                drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute('width')), parseInt(drawingCanvas.getAttribute('height')));
-                result.boxes.filter((box) => box !== result.box).forEach((box) => {
-                    Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: 'purple', lineWidth: 2 });
-                });
-            }
-            if (result.box) {
-                Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: 'blue', lineWidth: 2 });
-            }
-            if (result.codeResult && result.codeResult.code) {
-                // const validated = barcodeValidator(result.codeResult.code);
-                // const validated = validateBarcode(result.codeResult.code);
-                // Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: validated ? 'green' : 'red', lineWidth: 3 });
-                drawingCtx.font = "24px Arial";
-                // drawingCtx.fillStyle = validated ? 'green' : 'red';
-                // drawingCtx.fillText(`${result.codeResult.code} valid: ${validated}`, 10, 50);
-                drawingCtx.fillText(result.codeResult.code, 10, 20);
-                // if (validated) {
-                //     onDetected(result);
-                // }
-            }
-        }
-    };
-
-    useLayoutEffect(() => {
-        Quagga.init({
-            inputStream: {
-                type: 'LiveStream',
-                constraints: {
-                    ...constraints,
-                    ...(cameraId && { deviceId: cameraId }),
-                    ...(!cameraId && { facingMode }),
-                },
-                target: scannerRef.current,
-            },
-            locator,
-            numOfWorkers,
-            decoder: { readers: decoders },
-            locate,
-        }, (err) => {
-            Quagga.onProcessed(handleProcessed);
-
-            if (err) {
-                return console.log('Error starting Quagga:', err);
-            }
-            if (scannerRef && scannerRef.current) {
-                Quagga.start();
-                if (onScannerReady) {
-                    onScannerReady();
-                }
-            }
-        });
-        Quagga.onDetected(errorCheck);
-        return () => {
-            Quagga.offDetected(errorCheck);
-            Quagga.offProcessed(handleProcessed);
-            Quagga.stop();
-        };
-    }, [cameraId, onDetected, onScannerReady, scannerRef, errorCheck, constraints, locator, decoders, locate]);
-    return null;
-}
-
-Scanner.propTypes = {
+export class Scanner extends Component {
+  static propTypes = {
     onDetected: PropTypes.func.isRequired,
     scannerRef: PropTypes.object.isRequired,
     onScannerReady: PropTypes.func,
@@ -132,7 +40,140 @@ Scanner.propTypes = {
     locator: PropTypes.object,
     numOfWorkers: PropTypes.number,
     decoders: PropTypes.array,
-    locate: PropTypes.bool,
-};
+    locate: PropTypes.bool
+  };
+  static defaultProps = {
+    constraints: defaultConstraints,
+    locator: defaultLocatorSettings,
+    numOfWorkers: navigator.hardwareConcurrency || 0,
+    decoders: ["code_128_reader"],
+    locate: true,
+    frequency: 10
+  };
+  errorCheck = result => {
+    const { onDetected } = this.props;
+    if (!onDetected) {
+      return;
+    }
+    const err = getMedianOfCodeErrors(result.codeResult.decodedCodes);
+    console.log("on detected", result.codeResult, "error>>>", err);
+    // if Quagga is at least 75% certain that it read correctly, then accept the code.
+    if (err < 0.25) {
+      onDetected(result);
+    }
+  };
+
+  handleProcessed = result => {
+    const drawingCtx = Quagga.canvas.ctx.overlay;
+    const drawingCanvas = Quagga.canvas.dom.overlay;
+    drawingCtx.font = "24px Arial";
+    drawingCtx.fillStyle = "green";
+
+    if (result) {
+      // console.warn('* quagga onProcessed', result);
+      if (result.boxes) {
+        drawingCtx.clearRect(
+          0,
+          0,
+          parseInt(drawingCanvas.getAttribute("width")),
+          parseInt(drawingCanvas.getAttribute("height"))
+        );
+        result.boxes
+          .filter(box => box !== result.box)
+          .forEach(box => {
+            Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
+              color: "purple",
+              lineWidth: 2
+            });
+          });
+      }
+      if (result.box) {
+        Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, {
+          color: "blue",
+          lineWidth: 2
+        });
+      }
+      if (result.codeResult && result.codeResult.code) {
+        console.log("on processed", result.codeResult);
+        // const validated = barcodeValidator(result.codeResult.code);
+        // const validated = validateBarcode(result.codeResult.code);
+        // Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: validated ? 'green' : 'red', lineWidth: 3 });
+        // drawingCtx.font = "24px Arial";
+        // drawingCtx.fillStyle = validated ? 'green' : 'red';
+        // drawingCtx.fillText(`${result.codeResult.code} valid: ${validated}`, 10, 50);
+        // drawingCtx.fillText(result.codeResult.code, 10, 20);
+        // if (validated) {
+        //     onDetected(result);
+        // }
+      }
+    }
+  };
+
+  componentDidMount() {
+    const {
+      cameraId,
+      facingMode,
+      numOfWorkers,
+      frequency,
+      onScannerReady,
+      scannerRef,
+      constraints,
+      locator,
+      decoders,
+      locate
+    } = this.props;
+    Quagga.init(
+      {
+        inputStream: {
+          type: "LiveStream",
+          constraints: {
+            ...constraints,
+            ...(cameraId && { deviceId: cameraId }),
+            ...(!cameraId && { facingMode }),
+            aspectRatio: { min: 1, max: 100 }
+          },
+          target: scannerRef.current
+        },
+        locator,
+        numOfWorkers,
+        decoder: {
+          readers: decoders,
+          debug: {
+            drawBoundingBox: true,
+            showFrequency: true,
+            drawScanline: true,
+            showPattern: true
+          }
+        },
+        frequency,
+        locate
+      },
+      err => {
+        Quagga.onProcessed(this.handleProcessed);
+
+        if (err) {
+          return console.log("Error starting Quagga:", err);
+        }
+        if (scannerRef && scannerRef.current) {
+          Quagga.start();
+          if (onScannerReady) {
+            onScannerReady();
+          }
+        }
+      }
+    );
+    Quagga.onDetected(this.errorCheck);
+  }
+
+  componentWillUnmount() {
+    Quagga.offDetected(this.errorCheck);
+    Quagga.offProcessed(this.handleProcessed);
+    Quagga.stop();
+  }
+
+  render() {
+    return null;
+  }
+}
 
 export default Scanner;
